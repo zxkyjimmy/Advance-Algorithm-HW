53b8e6fc57dbdb9889538285ad5efbde6a145530
Yen-Chi Chen <zxkyjimmy@gmail.com>
2017-09-23 16:43:09 +0800

    show result

--- HW1/python/bf.py
+++ HW1/python/bf.py
@@ -1,9 +1,11 @@
 import sys
 import re
+import time
 def main():
     if len(sys.argv) != 2:
         print("Usage:", "python", sys.argv[0], "[input file]")
         return
+    startclock = time.clock()
     f = open(sys.argv[1], 'r')
     s = f.read()
     f.close()
@@ -14,8 +16,9 @@ def main():
         for j, rword in enumerate(words[i+1:]):
             if lword > rword:
                 entropy += 1
+    stopclock = time.clock()
     print("Total", len(words), "words")
-    print("entropy:", entropy)
-    
+    print("Entropy:", entropy)
+    print("Time:", stopclock-startclock)
 if __name__ == "__main__":
     main()
